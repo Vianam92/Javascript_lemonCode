4f5b35bf06d85ce0aade619d81c1f1b7db0c91ed
Mariana Viana <mariana.viana@kairosds.com>
2023-09-04 13:47:41 +0200

    add laboratorio modulo 3

--- laboratorio/typescript-sandbox/src/main.ts
+++ laboratorio/typescript-sandbox/src/main.ts
@@ -1,4 +1,11 @@
 import { dinner } from './modulo_2';
+import { createList, group1, group2, group3, group4, group5 } from './modulo_3';
 import "./style.css";
 
 console.log(dinner(120, 7, 18));
+
+createList(group1);
+createList(group2);
+createList(group3);
+createList(group4);
+createList(group5);
--- laboratorio/typescript-sandbox/src/modulo_3.ts
+++ laboratorio/typescript-sandbox/src/modulo_3.ts
@@ -1,8 +1,57 @@
-interface grupoMusical{
-    name: string,
-    cantante: string,
-    compositor: string,
-    fundation: number,
-    active: boolean,
-    género: string
+interface GrupoMusical {
+  name: string;
+  cantante?: string;
+  compositor?: string;
+  fundation: number;
+  active: boolean;
+  género: string;
 }
+
+export const group1: GrupoMusical = {
+  name: "The Beathles",
+  fundation: 1960,
+  active: true,
+  género: "🎵 Pop Rock",
+};
+
+export const group2: GrupoMusical = {
+  name: "Queen",
+  fundation: 1970,
+  active: false,
+  género: "🎸 Rock",
+};
+
+export const group3: GrupoMusical = {
+  name: "AC DC",
+  fundation: 1973,
+  active: true,
+  género: "🤘 Hard Rock",
+};
+
+export const group4: GrupoMusical = {
+  name: "Ludwig van Beethoven",
+  fundation: 1770,
+  active: false,
+  género: "🎼 Clásica",
+};
+
+export const group5: GrupoMusical = {
+  name: "The Rolling Stones",
+  fundation: 1962,
+  active: true,
+  género: "🎸 Rock",
+};
+
+export const createList = (group: any) => {
+  let list;
+  let elementLi;
+  list = document.querySelector(".list");
+  elementLi = document.createElement("li");
+  list?.appendChild(elementLi);
+  elementLi?.setAttribute("class", "element-class");
+  for (let item in group) {
+    const elementSpan = document.createElement("span");
+    elementLi.appendChild(elementSpan);
+    elementSpan.innerHTML += group[item];
+  }
+};
